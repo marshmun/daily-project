4f97d391782075e6612eb9f7ad63c639f8796b9b
Marshall Munoz <marshmun@gmail.com>
2017-07-06 16:59:53 -0500

    finished

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const mustacheExpress = require("mustache-express");
 mongoose.Promise = require("bluebird");
 const dbUrl = "mongodb://localhost:27017/pokemon";
 const Pokemon = require('./models/Pokemon');
 const app = express();
 const port = process.env.PORT || 8000;
 
+//ENGINE
+app.engine("mustache", mustacheExpress());
+app.set('views', './views');
+app.set("view engine", "mustache")
+
 //MIDDLEWARE
 app.use(bodyParser.json());
+app.use('/', express.static('./models'))
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 
 //DB CONNECTION
@@ -21,14 +30,55 @@ mongoose.connect(dbUrl).then((err, db) => {
 
 
 //ROUTES
-app.get('/pokemon', (req, res) => {
-    Pokemon.find().then(foundPokemon => {
-        res.send(foundPokemon);
+
+app.get('/', (req, res) => {
+    Pokemon.find({})
+        .then(foundPokemon => {
+            res.render('index', { pokemon: foundPokemon })
+        })
+})
+
+
+app.get('/update/:id', (req, res) => {
+    Pokemon.findOne({ _id: req.params.id })
+        .then(singlePokemon => {
+            res.render('update', { pokemon: singlePokemon })
+        })
+})
+
+app.post('/pokemon/:id', (req, res) => {
+    Pokemon.updateOne({ _id: req.params.id }, req.body)
+        .then(updatedPokemon => {
+            res.redirect('/')
+        })
+        .catch(err => {
+            res.status(500).send(err)
+        })
+})
+
+
+app.post('/', (req, res) => {
+    let pokemonData = req.body;
+    let newPokemon = new Pokemon(pokemonData);
+
+    newPokemon.save().then(savedPokemon => {
+        res.redirect('/')
     })
         .catch(err => {
             res.status(500).send(err);
         })
+});
+app.post('/delete', (req, res) => {
+    Pokemon.deleteOne({ _id: req.body.id })
+        .then(() => {
+            res.redirect("/");
+        })
+        .catch(err => {
+            res.status(500).send(err)
+        })
 })
+
+
 
 
 
